47904e973d09958b40c84c14e6a691a0d3129f7d
shriefkoush <shifoo242@gmail.com>
2024-12-14 20:16:57 +0200

    finished splash screen finished quran tab

--- lib/home/tabs/radio/Radio_Tab.dart
+++ lib/home/tabs/radio/Radio_Tab.dart
@@ -4,7 +4,9 @@ import 'package:flutter/material.dart';
 class Radio_Tab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return Container(child: Column(children: [
+
+    ],),
     );
   }
 }
